34cdf440ddbab0d19ac3d6f97dd1ffca91f841aa
WedgeManWik <j012100l@student.staffs.ac.uk>
2026-04-07 20:31:33 +0100

    update disclaimer

--- client/src/components/DisclaimerModal.tsx
+++ client/src/components/DisclaimerModal.tsx
@@ -33,7 +33,13 @@ const DisclaimerModal: React.FC<DisclaimerModalProps> = ({ show, onClose }) => {
              <strong><h2>WEBSITE DISCLAIMER</h2></strong>
           </div>
           <div className="MsoNormal" data-custom-class="body_text" style={{ lineHeight: '1.5' }}>
-            <span>The information provided by Macros100 is for general informational purposes only...</span>
+            <span>The information provided by Macros100 is for general informational purposes only. All information on the Site and our mobile application is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on the Site or our mobile application. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR OUR MOBILE APPLICATION OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE AND OUR MOBILE APPLICATION. YOUR USE OF THE SITE AND OUR MOBILE APPLICATION AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE AND OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.</span>
+          </div>
+          <div className="MsoNormal" data-custom-class="heading_1">
+            <strong><h2>PROFESSIONAL DISCLAIMER</h2></strong>
+          </div>
+          <div className="MsoNormal" data-custom-class="body_text" style={{ lineHeight: '1.5' }}>
+            <span>The Site cannot and does not contain medical/health advice. The medical/health information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of medical/health advice. THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON THE SITE OR OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.</span>
           </div>
         </div>
       </Modal.Body>
